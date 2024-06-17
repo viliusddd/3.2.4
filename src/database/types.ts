ae0094b7300e001c18e80c4580eec52acf405b73
Vilius <vilius@vilius.net>
2024-06-17 17:50:44 +0300

    chore: update types

--- src/database/types.ts
+++ src/database/types.ts
@@ -1,35 +1,56 @@
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
 export interface Directors {
-  movieId: number
-  personId: number
+  movieId: number;
+  personId: number;
 }
 
 export interface Movies {
-  id: number | null
-  title: string
-  year: number | null
+  id: number | null;
+  title: string;
+  year: number | null;
 }
 
 export interface People {
-  id: number | null
-  name: string
-  birth: number | null
+  birth: number | null;
+  id: number | null;
+  name: string;
 }
 
 export interface Ratings {
-  movieId: number
-  rating: number
-  votes: number
+  movieId: number;
+  rating: number;
+  votes: number;
+}
+
+export interface Screenings {
+  date: string;
+  id: Generated<number>;
+  movieId: number;
+  ticketsId: number;
 }
 
 export interface Stars {
-  movieId: number
-  personId: number
+  movieId: number;
+  personId: number;
+}
+
+export interface Tickets {
+  id: Generated<number>;
+  left: number;
+  total: number;
 }
 
 export interface DB {
-  directors: Directors
-  movies: Movies
-  people: People
-  ratings: Ratings
-  stars: Stars
+  directors: Directors;
+  movies: Movies;
+  people: People;
+  ratings: Ratings;
+  screenings: Screenings;
+  stars: Stars;
+  tickets: Tickets;
 }

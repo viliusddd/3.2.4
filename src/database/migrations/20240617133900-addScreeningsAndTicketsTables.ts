29db6c469e20bdff26bd2ee0d3850e78684482fa
Vilius <vilius@vilius.net>
2024-06-17 14:04:53 +0300

    fix: add primaryKey, autoIncrement to ids in up func, execute to down func

--- src/database/migrations/20240617133900-addScreeningsAndTicketsTables.ts
+++ src/database/migrations/20240617133900-addScreeningsAndTicketsTables.ts
@@ -5,26 +5,26 @@ export async function up(db: Kysely<SqliteDatabase>) {
   await db.schema
     .createTable('screenings')
     .ifNotExists()
-    .addColumn('id', 'integer', (c) => c.notNull())
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
     .addColumn('movie_id', 'integer', (c) =>
       c.notNull().references('movies.id')
     )
-    .addColumn('date', 'datetime', (c) => c.notNull())
     .addColumn('tickets_id', 'integer', (c) =>
       c.notNull().references('tickets.id')
     )
+    .addColumn('date', 'datetime', (c) => c.notNull())
     .execute()
 
   await db.schema
     .createTable('tickets')
     .ifNotExists()
-    .addColumn('id', 'integer', (c) => c.notNull())
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
     .addColumn('total', 'integer', (c) => c.notNull())
     .addColumn('left', 'integer', (c) => c.notNull())
     .execute()
 }
 
 export async function down(db: Kysely<SqliteDatabase>) {
-  db.schema.dropTable('screenings')
-  db.schema.dropTable('tickets')
+  await db.schema.dropTable('screenings').execute()
+  await db.schema.dropTable('tickets').execute()
 }
